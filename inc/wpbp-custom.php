8e41f6ecb468ad8d12ddb7caeff87674d1e879a8
Jonathan Roy <jroy@optimumweb.ca>
2016-12-17 01:08:17 -0500

    style

--- inc/wpbp-custom.php
+++ inc/wpbp-custom.php
@@ -4,11 +4,12 @@
 function enqueue() {
 	if ( !is_admin() ) {
 	    // libs
-	    wpbp_enqueue_lib(array('modernizr', 'jquery', 'wpbp'));
+	    wpbp_enqueue_lib(array( 'modernizr', 'jquery', 'wpbp' ));
 		// scripts
-		wp_enqueue_script('theme', THEME_URI . '/js/scripts.js', array('jquery'));
+		wp_enqueue_script('theme', THEME_URI . '/js/scripts.js', array( 'jquery' ));
 		// styles
-		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array('wpbp'));
+		wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:400,700');
+		wp_enqueue_style('theme', THEME_URI . '/css/master.css', array( 'wpbp' ));
 	}
 }
 add_action('init', 'enqueue');
