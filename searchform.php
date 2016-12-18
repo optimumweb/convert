64845d61fd69ca1298cc6ce66ddd42afa2db4e5a
Jonathan Roy <jroy@optimumweb.ca>
2016-12-18 02:14:06 -0500

    style

--- searchform.php
+++ searchform.php
@@ -1,10 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-    <div class="container">
-        <div class="grid_9">
-            <input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'wpbp'); ?>">
-        </div>
-        <div class="grid_3">
-            <input type="submit" id="searchsubmit" value="&#xf002;" class="button button-block">
-        </div>
-    </div>
+    <input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'wpbp'); ?>">
+    <input type="submit" id="searchsubmit" value="&#xf002;" class="button button-block">
 </form>
